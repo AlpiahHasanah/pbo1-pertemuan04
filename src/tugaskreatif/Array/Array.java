03228295a9dddba2eff08ca69e4905ece8808415
AlpiahHasanah <alpiahhasanah611@gmail.com>
2020-04-27 13:43:42 +0800

    tugas kreatif Array

---
+++ src/tugaskreatif/Array/Array.java
@@ -0,0 +1,8 @@
+package tugaskreatif.Array;
+
+public class Array {
+    public static void main(String[] args) {
+        int [] nilai ={1,2,3,4,5};
+        System.out.println(nilai[2]);
+    }
+}
