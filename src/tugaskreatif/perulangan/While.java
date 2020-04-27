0af86617611a0e259f8acb12ebfda7beba9a9811
AlpiahHasanah <alpiahhasanah611@gmail.com>
2020-04-27 13:45:32 +0800

    tugas kreatif While

---
+++ src/tugaskreatif/perulangan/While.java
@@ -0,0 +1,11 @@
+package tugaskreatif.perulangan;
+
+public class While {
+    public static void main(String[] args){
+        int x=0;
+        while (x<10){
+            System.out.println("Nilai x ="+x);
+            x++;
+        }
+    }
+}
