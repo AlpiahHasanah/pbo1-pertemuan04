6c411bca4d125fab5f9e1a917ad58d33943a54a4
AlpiahHasanah <alpiahhasanah611@gmail.com>
2020-04-27 13:44:40 +0800

    tugas kreatif DoWhile

---
+++ src/tugaskreatif/perulangan/DoWhile.java
@@ -0,0 +1,11 @@
+package tugaskreatif.perulangan;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        int x = 0;
+        do {
+            System.out.println("Nilai x =" + x);
+            x++;
+        } while (x < 10);
+    }
+}
