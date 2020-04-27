be48bab7b3ae5b3d095d9689de64314a96a9aaca
AlpiahHasanah <alpiahhasanah611@gmail.com>
2020-04-27 13:45:13 +0800

    tugas kreatif For

---
+++ src/tugaskreatif/perulangan/For.java
@@ -0,0 +1,11 @@
+package tugaskreatif.perulangan;
+
+public class For {
+    public static void main(String[] args){
+        int x=0;
+        for (x=1;x<10;x++){
+            System.out.println("Nilai x ="+x);
+        }
+    }
+
+}
